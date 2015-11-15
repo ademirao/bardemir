efebd133e494ea2fd0ab1133b08c0cf31566b453
Ademir de Alvarenga Oliveira <ademirao@gmail.com>
2015-11-14 22:56:56 -0200

    Add login page and facebook login flow

--- api.py
+++ api.py
@@ -5,6 +5,7 @@ as well as those methods defined in an API.
 """
 
 import endpoints
+from google.appengine.api import urlfetch
 from protorpc import messages
 from protorpc import message_types
 from protorpc import remote
@@ -28,10 +29,16 @@ class EventsCollection(messages.Message):
     items = messages.MessageField(Event, 1, repeated=True)
 
 
+
 STORED_EVENTS = EventsCollection(items=[
     Event(description='Event 1'),
     Event(description='Event 2'),
 ])
+
+# url = "http://www.facebook.com/dialog/oauth?client_id=433202543531394&redirect_url=http://bardemir-api.appspot.com"
+#	result = urlfetch.fetch(url)
+#event = Event()
+#	event.description = result.content
 
 @endpoints.api(name='bardemir', version='v1',
                allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
@@ -40,11 +47,19 @@ STORED_EVENTS = EventsCollection(items=[
 class BardemirService(remote.Service):
     """Bardemir API v1."""
 
-    @endpoints.method(message_types.VoidMessage, EventsCollection,
-                      path='listEvents', http_method='GET',
+    AUTH_RESOURCE = endpoints.ResourceContainer(
+            message_types.VoidMessage,
+            auth=messages.StringField(1))
+
+    @endpoints.method(AUTH_RESOURCE, Event,
+                      path='listEvents', http_method='POST',
                       name='events.list')
-    def listEvents(self, unused_request):
-        return STORED_EVENTS
+    def listEvents(self, request):
+	url = "http://graph.facebook.com/v2.5/me?access_token=%s" % request.auth
+	result = urlfetch.fetch(url)
+	event = Event()
+	event.description = result.content
+	return event
 
     ID_RESOURCE = endpoints.ResourceContainer(
             message_types.VoidMessage,
